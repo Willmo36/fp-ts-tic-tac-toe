557fa8834268a6ee352cfd3e6aed9b83b4186692
Max Willmott <max.willmo@gmail.com>
2019-12-14 17:25:36 -0600

    Initial loop

---
+++ src/StateTask.ts
@@ -0,0 +1,5 @@
+import { StateT } from "fp-ts/lib/StateT";
+import { Task } from "fp-ts/lib/Task";
+
+sta
+sta
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,23 @@ import * as Array from "fp-ts/lib/Array";
 import { Do } from "fp-ts-contrib/lib/Do";
 import { pipe } from "fp-ts/lib/pipeable";
 import { flow, identity } from "fp-ts/lib/function";
+import * as STE from "fp-ts-contrib/lib/StateTaskEither";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as Either from "fp-ts/lib/Either";
+import * as Task from "fp-ts/lib/Task";
+import Realine from "readline";
+import { Show } from "fp-ts/lib/Show";
 
 type Player = "x" | "o";
+
+type GameState = {
+  lastMove: Player;
+  board: Board;
+};
+
 type CellState = Option.Option<Player>;
 
-type GameState = [
+type Board = [
   CellState,
   CellState,
   CellState,
@@ -20,27 +32,45 @@ type GameState = [
 ];
 
 type Position = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+const showPosition: Show<Position> = {
+  show: p => (p+1).toString()
+}
 
-const initialGameState: GameState = [
-  Option.none,
-  Option.none,
-  Option.none,
-  Option.none,
-  Option.none,
-  Option.none,
-  Option.none,
-  Option.none,
-  Option.none,
-];
+const initialGameState: GameState = {
+  lastMove: "o",
+  board: [
+    Option.none,
+    Option.none,
+    Option.none,
+    Option.none,
+    Option.none,
+    Option.none,
+    Option.none,
+    Option.none,
+    Option.none
+  ]
+};
 
+const showCellState = (board: Board, position: Position) => pipe(
+  board[position], 
+  Option.fold(() => showPosition.show(position), identity)
+);
 
-const showCellState = Option.fold(() => " ", identity);
+const rl = Realine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-function setGameState(
-  gs: GameState,
-  position: Position,
-  player: Player
-): GameState {
+const question = (q: string): Task.Task<string> => () => {
+  return new Promise(res =>
+    rl.question(q, answer => {
+      rl.pause();
+      res(answer);
+    })
+  );
+};
+
+function setGameState(gs: Board, position: Position, player: Player): Board {
   const update = (cellPos: Position) =>
     cellPos === position ? Option.some(player) : gs[cellPos];
 
@@ -61,33 +91,53 @@ function setGameState(
  * Try to commit the move, failing if the cell has already been taken
  */
 function commitMove(
-  gs: GameState,
+  gs: Board,
   position: Position,
   player: Player
-): Option.Option<GameState> {
+): Either.Either<Position, Board> {
   if (Option.isSome(gs[position])) {
-    return Option.none;
+    return Either.left(position);
   }
 
-  return Option.some(setGameState(gs, position, player));
+  return Either.right(setGameState(gs, position, player));
 }
 
-function loop(gs: GameState){
+function loop(gs: GameState): TE.TaskEither<any, GameState> {
+  const player = gs.lastMove === "x" ? "o" : "x";
+  console.clear();
+  render(gs.board);
 
-
+  return pipe(
+    TE.taskEither.fromTask<any, string>(question("Choose cell: ")),
+    TE.chain(position =>
+      pipe(
+        commitMove(gs.board, <Position>(Number(position) - 1), player),
+        TE.fromEither,
+        TE.map<Board, GameState>(board => ({ board, lastMove: player }))
+      )
+    ),
+    TE.alt(() => loop(gs)),
+    TE.chain(nextGS => {
+      //if is complete stop
+      return loop(nextGS);
+    })
+  );
 }
-
 
 //credit to https://flaviocopes.com/javascript-template-literals/
-const drawGrid = (gs: GameState) => (literals: TemplateStringsArray, ...positions: Position[]) => {
-  return positions.reduce((acc, pos, i) => {
-    return acc + literals[i] + ` ${showCellState(gs[pos])}  `;
-  }, "")+ literals[9];
+const renderBoard = (board: Board) => (
+  literals: TemplateStringsArray,
+  ...positions: Position[]
+) => {
+  return (
+    positions.reduce((acc, pos, i) => {
+      return acc + literals[i] + ` ${showCellState(board, pos)}  `;
+    }, "") + literals[9]
+  );
 };
 
-
-function render(gs: GameState) {
-  const frame = drawGrid(gs)`
+function render(gs: Board) {
+  const frame = renderBoard(gs)`
    _____________________________
   |         |         |         |
   |   ${0}  |   ${1}  |   ${2}  |
@@ -101,6 +151,6 @@ function render(gs: GameState) {
     `;
 
   console.log(frame);
-};
+}
 
-render(initialGameState)
+loop(initialGameState)();
